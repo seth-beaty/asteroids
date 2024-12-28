a5e42298709219158e850c7ba7367832aff2b14e
seth-beaty <seth.a.beaty@gmail.com>
2024-12-28 18:55:42 -0500

    2: Add player class and implement rotation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,13 +10,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    game_clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        dt = game_clock.tick(60) / 1000
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
