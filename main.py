82f45aa15e27475dfccb85867eb9a82924ebd295
seth-beaty <seth.a.beaty@gmail.com>
2024-12-28 19:12:50 -0500

    4: Added grouping for game objects

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from player import Player
 
 
 def main():
-    print("Starting asteroids!")
     pygame.init()
+    print("Starting asteroids!")  
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -20,8 +25,13 @@ def main():
                 return
         screen.fill((0,0,0))
         dt = game_clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
+
+        for object in updateable:
+            object.update(dt)
+        
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
         
 
